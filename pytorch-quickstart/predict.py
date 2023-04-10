b97e902df5e246949ca5e1a016ff5037a5d36ee2
Jan Deinhard <jan.deinhard@gmail.com>
2023-04-10 07:40:05 +0200

    add pytorch quickstart code

---
+++ pytorch-quickstart/predict.py
@@ -0,0 +1,35 @@
+import torch
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from NeuralNetwork import NeuralNetwork
+
+model = NeuralNetwork()
+model.load_state_dict(torch.load("model.pth"))
+
+# Download test data from open datasets.
+test_data = datasets.FashionMNIST(
+    root="data",
+    train=False,
+    download=True,
+    transform=ToTensor(),
+)
+
+classes = [
+    "T-shirt/top",
+    "Trouser",
+    "Pullover",
+    "Dress",
+    "Coat",
+    "Sandal",
+    "Shirt",
+    "Sneaker",
+    "Bag",
+    "Ankle boot",
+]
+
+model.eval()
+x, y = test_data[0][0], test_data[0][1]
+with torch.no_grad():
+    pred = model(x)
+    predicted, actual = classes[pred[0].argmax(0)], classes[y]
+    print(f'Predicted: "{predicted}", Actual: "{actual}"')
